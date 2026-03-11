49707227af0fbe7d2c610678e3b708c3c581adae
Sandile Thabede <thabethesandile44@gmail.com>
2026-03-11 17:54:11 +0200

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const router = express.Router()
+
+const { searchBusinesses } = require("./scraper")
+
+
+// search businesses
+router.get("/search", async (req,res)=>{
+
+const industry = req.query.industry
+const city = req.query.city
+
+const results = await searchBusinesses(industry,city)
+
+res.json(results)
+
+})
+
+
+// save lead
+router.post("/lead",(req,res)=>{
+
+const lead = req.body
+
+console.log("Saved lead:",lead)
+
+res.json({
+message:"Lead stored",
+lead
+})
+
+})
+
+
+module.exports = router
