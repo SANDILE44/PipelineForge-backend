46b145f4e5bb9049b622ac0568cf62f93b45a0d2
Sandile Thabede <thabethesandile44@gmail.com>
2026-03-12 04:14:13 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -3,66 +3,30 @@ const cheerio = require("cheerio")
 
 async function searchBusinesses(industry, city){
 
-let businesses = []
+ const query = `${industry} companies in ${city}`
+ const url = `https://www.bing.com/search?q=${encodeURIComponent(query)}`
 
-try{
+ const response = await axios.get(url)
 
-const query = `${industry} ${city}`
+ const $ = cheerio.load(response.data)
 
-const response = await axios.get(`https://www.bing.com/search?q=${encodeURIComponent(query)}`)
+ const businesses = []
 
-const $ = cheerio.load(response.data)
+ $("li.b_algo h2 a").each((i,el)=>{
 
-let links = []
+  const name = $(el).text()
+  const website = $(el).attr("href")
 
-$("li.b_algo h2 a").each((i,el)=>{
-if(i < 10){
-links.push($(el).attr("href"))
-}
-})
+  businesses.push({
+   name,
+   website,
+   industry,
+   city
+  })
 
-for(let link of links){
+ })
 
-try{
-
-const page = await axios.get(link)
-
-const html = page.data
-
-const emailMatch = html.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/ig)
-
-let email = emailMatch ? emailMatch[0] : "Not found"
-
-businesses.push({
-name: link.replace("https://",""),
-email: email,
-phone: "Not found",
-website: link,
-industry: industry,
-city: city
-})
-
-}catch(err){
-
-businesses.push({
-name: link.replace("https://",""),
-email: "Not found",
-phone: "Not found",
-website: link,
-industry: industry,
-city: city
-})
-
-}
-
-}
-
-}catch(error){
-console.log(error)
-}
-
-return businesses
-
+ return businesses
 }
 
 module.exports = { searchBusinesses }
