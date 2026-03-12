70fa6facea94c71da3439f55069bb5449549966b
Sandile Thabede <thabethesandile44@gmail.com>
2026-03-12 11:40:15 +0200

    Create industries.js

---
+++ services/industries.js
@@ -0,0 +1,31 @@
+const industries = [
+
+"logistics",
+"transport",
+"freight",
+"courier",
+"construction",
+"plumbing",
+"electrical",
+"cleaning services",
+"security company",
+"marketing agency",
+"digital marketing",
+"software company",
+"IT services",
+"web design",
+"accounting firm",
+"law firm",
+"real estate agency",
+"car dealership",
+"car rental",
+"auto repair",
+"printing services",
+"event planning",
+"photography",
+"catering services",
+"restaurant suppliers"
+
+]
+
+module.exports = industries
